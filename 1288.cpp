0d438525295402e3c294879b34175393256b9de4
Akshat Aswal <akshataswal84@gmail.com>
2026-07-06 23:12:28 +0530

    1288. Remove Covered Intervals

---
+++ 1288.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+
+        int remaining = intervals.size();
+
+        for (int i = 0; i < intervals.size(); i++) {
+
+            for (int j = 0; j < intervals.size(); j++) {
+
+                if (i == j) continue;
+                if (intervals[j][0] <= intervals[i][0] &&
+                    intervals[i][1] <= intervals[j][1]) {
+                    remaining--;
+                    break;
+                }
+            }
+        }
+        return remaining;
+    }
+};
